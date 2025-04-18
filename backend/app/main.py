30f04fa639979bea8943d62f7313011320777f22
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-18 23:29:48 +0400

    minor

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,14 +2,10 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.api.routes import router
 from app.db.database import Base, engine
-from app.db.timescale import init_db_pool
 import logging
-
-pool = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global pool
     # Startup logic
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
