79b9d6b703b0de99035dc71480ba53cbd6997f26
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-18 22:26:05 +0400

    small fix to seed bash script

--- backend/seed.sh
+++ backend/seed.sh
@@ -1,5 +1,5 @@
 docker cp ./app/db/seed_wells.sql postgres_db:/seed_wells.sql
 docker-compose exec db psql -U postgres -d wells_db -f /seed_wells.sql
 
-docker cp ./app/db/seed_timescale.sql timescale_db:/
+docker cp ./app/db/seed_timescale.sql timescale_db:/seed_timescale.sql
 docker-compose exec timescaledb psql -U timescale -d timescale_db -f /seed_timescale.sql
