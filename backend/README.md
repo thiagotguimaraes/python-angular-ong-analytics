c88eee35025df2869a68c4a1f92d3e745987fc2f
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-18 22:19:14 +0400

    creating first draft README

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+cd backend
+docker-compose up
+<!-- Wait for databases to start -->
+bash seed.sh
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from app.api.routes import router
 from app.db.database import Base, engine
 from app.db.database import SessionLocal
-from app.db.mock_data import initialize_database
 
 app = FastAPI()
 
@@ -10,7 +9,5 @@ app = FastAPI()
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-        db = SessionLocal()
-        # await initialize_database(db)
 
 app.include_router(router)
