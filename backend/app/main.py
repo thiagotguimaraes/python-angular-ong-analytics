2488ea7c64720df617707a68b74e80cb211ad02b
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-19 11:50:36 +0400

    adding cors middleware to fastpi backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.api.routes import router
 from app.db.timescale_db_manager import ts_db_manager
@@ -24,5 +25,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # or ["*"] for all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
